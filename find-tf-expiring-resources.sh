499bfdd05f6b20d31caf998ca64f9d7a3db6b470
Ming Yu <ming.yu@microsoft.com>
2021-01-28 14:56:42 -0600

    add echos

--- find-tf-expiring-resources.sh
+++ find-tf-expiring-resources.sh
@@ -1,12 +1,22 @@
+#########################################################################################
+# This script uses az and jq CLI to search for service principals with expiring resources
+# listed in the terraform state file.  
+#
+# Usage: ./find-tf-expiring-resources.sh 1m
+# Parameter '1m' is the time span, 1 month in this case, from now that the credentials 
+# will expire by.
+# 
+#########################################################################################
+
 devFile=fg-global.tfstateenv.dev
 stgFile=fg-global.tfstateenv.stg 
 prodFile=fg-global.tfstateenv.prod 
 
 dateToExpire="$(date -v +$1  +%Y-%m-%d)"
+
+#The web hook used by Flipgrid-Platform team's Hooks channel
 url=https://outlook.office.com/webhook/e7e77091-2a55-48dd-b6f3-974e04ffcc39@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/dd854d91bc4744d694b61c05187633b0/dae8bb79-9600-4c4f-90f8-6403e65e5bd4?files=2
 
-
-echo $dateToExpire
 
 az storage blob download \
 --account-name fgterraform \
@@ -29,15 +39,15 @@ az storage blob download \
 --name "fg-global.tfstateenv:prod" \
 --file "./$prodFile" -o none
 
-echo "Searching expiring resources by $dateToExpire in dev env=..."
+echo "Searching terraform managed resources that will expire by $dateToExpire in dev env..."
 devResult=$(jq --arg EXPIRE_DATE "$dateToExpire" '.resources[] | select( .. | .end_date? | select (. < $EXPIRE_DATE and . != null )) | .type + " -- " + .name' $devFile | jq -s .)
 echo $devResult
 
-echo "Searching expiring resources by $dateToExpire in stg env..."
+echo "Searching terraform managed resources that will expire by $dateToExpire in stg env..."
 stgResult=$(jq --arg EXPIRE_DATE "$dateToExpire" '.resources[] | select( .. | .end_date? | select (. < $EXPIRE_DATE and . != null )) | .type + " -- " + .name' $stgFile | jq -s .)
 echo $stgResult 
 
-echo "Searching expiring resources by $dateToExpire in prod env..."
+echo "Searching terraform managed resources that will expire by $dateToExpire in prod env..."
 prodResult=$(jq --arg EXPIRE_DATE "$dateToExpire" '.resources[] | select( .. | .end_date? | select (. < $EXPIRE_DATE and . != null )) | .type + " -- " + .name' $prodFile | jq -s .)
 echo $prodResult 
 
@@ -45,7 +55,7 @@ rm $devFile
 rm $stgFile
 rm $prodFile
 
-#adding <pre></pre> to remove markdown
+#adding <pre></pre> to remove markdown when posting to web hook 
 if [ "$devResult" != "[]" ]; then  
     curl -s -H "Content-Type:application/json" -X POST --data "{'title': 'Found expiring resources by $dateToExpire from terraform state - dev', 'text':'<pre>$devResult</pre>'}" $url 
 fi
